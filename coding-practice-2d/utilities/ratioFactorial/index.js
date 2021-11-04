264579566011f83cad24f9c723dca3e7a00873e8
ussvarma <93523488+ussvarma@users.noreply.github.com>
2021-11-04 15:55:27 +0530

    Update index.js

--- coding-practice-2d/utilities/ratioFactorial/index.js
+++ coding-practice-2d/utilities/ratioFactorial/index.js
@@ -2,7 +2,7 @@ const factorial=require("../factorial/index")
 const ratio=require("../ratio/index")
 const ratioAndFactorial=(num1,num2,num3)=>{
     return{
-        ratio:ratio(num1,num2)
+        ratio:ratio(num1,num2),
         factorial:factorial(num3)
     }
 }
